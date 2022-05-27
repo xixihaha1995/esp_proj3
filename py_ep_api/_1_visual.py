10ffd6a59fa0d2a8e4fc93e7269898f04a574ec4
Lichen Wu <43649779+xixihaha1995@users.noreply.github.com>
2022-05-26 20:14:20 -0600

    solar radiation done

--- py_ep_api/_1_visual.py
+++ py_ep_api/_1_visual.py
@@ -23,20 +23,21 @@ for time_step in range(np.array(result['oat'][drop_warmup:]).shape[0]):
     x_date.append(datetime_time)
     x_date_start = x_date_start + 15*60
 
-
-
 fig, ax1 = plt.subplots()
 ax2 = ax1.twinx()
 
 ax1.plot(x_date,np.array(result['oat'][drop_warmup:]) , color = next(colorcycler), linestyle = next(linecycler),
          label = "Outdoor air temperature [C]",linewidth = line_width)
-ax2.plot(x_date, result['elec_hvac'][drop_warmup:],color = next(colorcycler), linestyle = next(linecycler),
-         label = "Electricity:HVAC [J]",linewidth = line_width)
+# ax2.plot(x_date, result['elec_hvac'][drop_warmup:],color = next(colorcycler), linestyle = next(linecycler),
+#          label = "Electricity:HVAC [J]",linewidth = line_width)
+ax2.plot(x_date, result['solar'][drop_warmup:],color = next(colorcycler), linestyle = next(linecycler),
+         label = "Direct Solar Radiation Rate per Area [W/m2]",linewidth = line_width)
 # plt.ylabel("Zone Air System Sensible Cooling Rate (W)",fontsize=large_font)
 plt.tick_params(axis='both', which='major', labelsize=large_font)
 
 ax1.set_ylabel("Temperature [C]",fontsize=large_font)
-ax2.set_ylabel("Electricity [J]",fontsize=large_font)
+# ax2.set_ylabel("Electricity [J]",fontsize=large_font)
+ax2.set_ylabel("Solar Radiation [W/m2]",fontsize=large_font)
 h1, l1 = ax1.get_legend_handles_labels()
 h2, l2 = ax2.get_legend_handles_labels()
 ax1.legend(h1 + h2, l1 + l2)
--- py_ep_api/pyep_read.py
+++ py_ep_api/pyep_read.py
@@ -7,28 +7,33 @@ outdoor_temp_sensor = 0
 outdoor_dew_point_actuator = 0
 oat_lst = []
 ele_hvac_lst = []
+solar_lst = []
 def time_step_handler(state):
-    global one_time , oat_handle,  elec_hvac_handle, zoon_cool_sensor
+    global one_time , oat_handle,  elec_hvac_handle, solar_handle, zoon_cool_sensor
     if one_time:
         if not api.exchange.api_data_fully_ready(state):
             return
         oat_handle = api.exchange.get_variable_handle(
             state, u"SITE OUTDOOR AIR DRYBULB TEMPERATURE", u"ENVIRONMENT")
         elec_hvac_handle = api.exchange.get_meter_handle(state, "Electricity:HVAC")
+        solar_handle = api.exchange.get_variable_handle(
+            state, u"Site Direct Solar Radiation Rate per Area", u"ENVIRONMENT")
         # zoon_cool_sensor = api.exchange.get_variable_handle(state, u"Zone Air System Sensible Cooling Rate", u"SPACE1-1")
         one_time = False
     oat_val = api.exchange.get_variable_value(state, oat_handle)
     elec_hvac_value = api.exchange.get_meter_value(state, elec_hvac_handle)
+    solar_val = api.exchange.get_variable_value(state, solar_handle)
     oat_lst.append(oat_val)
     ele_hvac_lst.append(elec_hvac_value)
+    solar_lst.append(solar_val)
 
 api = EnergyPlusAPI()
 state = api.state_manager.new_state()
 api.runtime.callback_end_zone_timestep_after_zone_reporting(state, time_step_handler)
 api.runtime.run_energyplus(state, sys.argv[1:])
-print(len(ele_hvac_lst))
 result = util.loadJSONFromOutputs('pyep_results')
 result["oat"] = oat_lst
 result["elec_hvac"] = ele_hvac_lst
+result["solar"] = solar_lst
 util.saveJSON(result, "pyep_results")
 
