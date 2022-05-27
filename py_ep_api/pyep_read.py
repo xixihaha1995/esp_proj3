44e22a73f6c5381f041ea1b54ed28d68c09046ea
Lichen Wu <43649779+xixihaha1995@users.noreply.github.com>
2022-05-26 18:29:17 -0600

    meter handle and value got

--- py_ep_api/pyep_read.py
+++ py_ep_api/pyep_read.py
@@ -7,21 +7,17 @@ outdoor_temp_sensor = 0
 outdoor_dew_point_actuator = 0
 zone_cool = []
 def time_step_handler(state):
-    global one_time , outdoor_temp_sensor , outdoor_dew_point_sensor , outdoor_dew_point_actuator,zoon_cool_sensor
+    global one_time , outdoor_temp_sensor, zoon_cool_sensor, elec_hvac
     if one_time:
         if not api.exchange.api_data_fully_ready(state):
             return
         outdoor_temp_sensor = api.exchange.get_variable_handle(
             state, u"SITE OUTDOOR AIR DRYBULB TEMPERATURE", u"ENVIRONMENT")
-        zoon_cool_sensor = api.exchange.get_variable_handle(
-            state, u"Zone Air System Sensible Cooling Rate", u"SPACE1-1")
-        outdoor_dew_point_actuator = api.exchange.get_actuator_handle(
-            state, "Weather Data", "Outdoor Dry Bulb", "Environment")
+        elec_hvac = api.exchange.get_meter_handle(state, "Electricity:HVAC")
         one_time = False
     oa_temp = api.exchange.get_variable_value(state, outdoor_temp_sensor)
-    zone_cool_rate = api.exchange.get_variable_value(state, zoon_cool_sensor)
+    zone_cool_rate = api.exchange.get_meter_value(state, elec_hvac)
     zone_cool.append(zone_cool_rate)
-    api.exchange.set_actuator_value(state, outdoor_dew_point_actuator, -5)
 
 def saveJSON(data, name):
     script_dir = os.path.dirname(__file__)  # <-- absolute dir the script is in
@@ -37,8 +33,8 @@ def loadJSONFromOutputs(name):
 api = EnergyPlusAPI()
 state = api.state_manager.new_state()
 api.runtime.callback_end_zone_timestep_after_zone_reporting(state, time_step_handler)
-api.exchange.request_variable(state, "SITE OUTDOOR AIR DRYBULB TEMPERATURE", "ENVIRONMENT")
 api.runtime.run_energyplus(state, sys.argv[1:])
+print(zone_cool)
 # result = loadJSONFromOutputs('pyep_results')
 # result["changed"] = zone_cool
 # saveJSON(result, "pyep_results")
