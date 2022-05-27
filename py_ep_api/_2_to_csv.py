98977bff3966500a9a25d74bf4038053ff32092e
Lichen Wu <43649779+xixihaha1995@users.noreply.github.com>
2022-05-27 10:28:19 -0600

    Update _2_to_csv.py
    debug the timestamp

--- py_ep_api/_2_to_csv.py
+++ py_ep_api/_2_to_csv.py
@@ -1,2 +1,15 @@
 import _0_util as util
 result = util.loadJSONFromOutputs('pyep_results')
+
+df = pd.read_csv(case_arr_abs)
+df.Timestamp = pd.to_datetime(df.Timestamp)
+df['minute'] = df.apply(lambda x: x['Timestamp'].minute, axis=1)
+df['hour'] = df.apply(lambda x: x['Timestamp'].hour, axis=1)
+df['dayofweek'] = df.apply(lambda x: x['Timestamp'].dayofweek, axis=1)
+new_cols = ['dayofweek', 'hour', 'minute', 't_out', 't_slab1',
+            't_cav', 'valve_ht', 'valve_cl', 'vfr_water', 'rc_y', 'y']
+
+df = df.sort_values("Timestamp").drop("Timestamp", axis=1)
+# new_cols = ['t_out','t_slab1','t_cav','valve_ht',
+#             'valve_cl','vfr_water','rc_y','y']
+df = df[new_cols]
