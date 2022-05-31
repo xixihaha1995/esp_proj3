7d1bb3cecffaf321ef045729d4634318d3fc859f
Lichen Wu <43649779+xixihaha1995@users.noreply.github.com>
2022-05-31 10:11:02 -0600

    Update _2_to_csv.py

--- py_ep_api/_2_to_csv.py
+++ py_ep_api/_2_to_csv.py
@@ -27,31 +27,50 @@ train_x = []
 train_y = []
 test_x = {}
 test_y = {}
+
+'''
+Train df,
+Test df
+'''
+test_portion = int(test_portion_factor * len(df.values))
+df_train = df.iloc[:-test_portion]
+df_test = df.iloc[-test_portion:]
 # Scaling the input data
 sc = MinMaxScaler()
 label_sc = MinMaxScaler()
-data = sc.fit_transform(df.values)
+data_train = sc.fit_transform(df_train.values)
+data_test = sc.transform(df_test.values)
 # Obtaining the Scale for the labels(usage data) so that output can be re-scaled to actual value during evaluation
-label_sc.fit(df.iloc[:, 0].values.reshape(-1, 1))
+label_sc.fit(df_train.iloc[:, -1].values.reshape(-1, 1))
 label_scalers[case_arr_abs] = label_sc
 
 # Use lookback period to split inputs/labels
-inputs = np.zeros((len(data) - lookback, lookback, df.shape[1]))
-labels = np.zeros(len(data) - lookback)
+inputs_train = np.zeros((len(data_train) - lookback, lookback, df_train.shape[1]))
+labels_train = np.zeros(len(data_train) - lookback)
 
-for i in range(lookback, len(data)):
-    inputs[i - lookback] = data[i - lookback:i]
-    labels[i - lookback] = data[i, 0]
-inputs = inputs.reshape(-1, lookback, df.shape[1])
-labels = labels.reshape(-1, 1)
+for i in range(lookback, len(data_train)):
+    inputs_train[i - lookback] = data_train[i - lookback:i]
+    labels_train[i - lookback] = data_train[i, 0]
+inputs_train = inputs_train.reshape(-1, lookback, df_train.shape[1])
+labels_train = labels_train.reshape(-1, 1)
 
+
+inputs_test = np.zeros((len(data_test) , lookback, df_test.shape[1]))
+labels_test = np.zeros(len(data_test) )
+scaled_tain_test_df = np.concatenate((data_train,data_test))
+for i in range(data_train.shape[0] , len(scaled_tain_test_df)):
+    inputs_test[i - data_train.shape[0]] = scaled_tain_test_df[i - lookback:i]
+    labels_test[i - data_train.shape[0]] = scaled_tain_test_df[i, 0]
+inputs_test = inputs_test.reshape(-1, lookback, df_test.shape[1])
+labels_test = labels_test.reshape(-1, 1)
 # Split data into train/test portions and combining all data from different files into a single array
-test_portion = int(test_portion_factor * len(inputs))
 if len(train_x) == 0:
-    train_x = inputs[:-test_portion]
-    train_y = labels[:-test_portion]
+    train_x = inputs_train[:]
+    train_y = labels_train[:]
 else:
-    train_x = np.concatenate((train_x, inputs[:-test_portion]))
-    train_y = np.concatenate((train_y, labels[:-test_portion]))
-test_x[case_arr_abs] = (inputs[-test_portion:])
-test_y[case_arr_abs] = (labels[-test_portion:])
+    train_x = np.concatenate((train_x, inputs_train[:]))
+    train_y = np.concatenate((train_y, labels_train[:]))
+
+test_x[case_arr_abs] = (inputs_test[:])
+test_y[case_arr_abs] = (labels_test[:])
+pass
