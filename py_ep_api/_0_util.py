a1c5209e3ba03081df462ce55a39917de33688e8
Lichen Wu <43649779+xixihaha1995@users.noreply.github.com>
2022-06-02 17:06:34 -0600

    toy 2 generating

--- py_ep_api/_0_util.py
+++ py_ep_api/_0_util.py
@@ -1,12 +1,12 @@
 import os, json
 def saveJSON(data, name):
     script_dir = os.path.dirname(__file__)  # <-- absolute dir the script is in
-    with open(os.path.join(script_dir, '../py_ep_outputs',name + '.json'), 'w', encoding='utf-8') as f:
+    with open(os.path.join(script_dir, '../toy_2_py_ep_outputs',name + '.json'), 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
 def loadJSONFromOutputs(name):
     script_dir = os.path.dirname(__file__)  # <-- absolute dir the script is in
-    with open(os.path.join(script_dir, '../py_ep_outputs',name + '.json'), 'r') as f:
+    with open(os.path.join(script_dir, '../toy_2_py_ep_outputs',name + '.json'), 'r') as f:
         try:
             testDict = json.loads(f.read())
         except json.decoder.JSONDecodeError:
